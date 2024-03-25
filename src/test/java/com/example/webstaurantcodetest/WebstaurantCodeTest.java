1aff0c37fe7001b0c4d74ca04d9898dd2934df22
Garrett <garrett.a.krotee@gmail.com>
2024-03-25 00:10:34 -0400

    Renaming directory

--- src/test/java/com/example/webstaurantcodetest/WebstaurantCodeTest.java
+++ src/test/java/com/example/webstaurantcodetest/WebstaurantCodeTest.java
@@ -1,4 +1,4 @@
-package com.example.webstaurantcodetestjava;
+package com.example.webstaurantcodetest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
